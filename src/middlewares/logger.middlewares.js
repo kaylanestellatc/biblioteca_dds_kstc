6d582fa882ce129708df08b6e9815ad2eef7a050
KaylaneStellaTC <kaylanestella73@gmail.com>
2026-04-14 10:09:57 -0300

    pequenas mudanças

--- src/middlewares/logger.middlewares.js
+++
@@ -1,8 +0,0 @@
-const logger = (req, res, next) => {
-    const timestamp = new Date().toISOString();
-    console.log(`[BIBLIOTECA] ${timestamp} | ${req.method} ${req.url}`);
-    next(); // ← OBRIGATÓRIO: passa para o próximo posto
-  };
-  
-  module.exports = logger;
-  
--- src/routes/usuarios.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usuarioController = require('../controllers/usuarios.controller');
-
-router.get('/', usuarioController.listarUsuarios);
-router.get('/:id', usuarioController.buscarUsuarioPorId);
-router.post('/', usuarioController.criarUsuario);
-
-module.exports = router;
